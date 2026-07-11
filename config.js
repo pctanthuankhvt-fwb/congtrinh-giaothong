2f451a99a77bfad6b7783aec86d6bcdb40d46b92
pctanthuankhvt-fwb <pctanthuankhvt@gmail.com>
2026-07-12 04:49:54 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,3 @@
+// Cấu hình kết nối Supabase
+const SUPABASE_URL = 'https://ogsmeqzaadbptkfihpll.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_jiemX5EFR6BsTl6Kjne1BQ_wB50syVO'; // Publishable Key
